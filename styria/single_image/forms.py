a144fa2338b097dcc41a3200aa73f25af43915c8
mislavcimpersak <mislav.cimpersak@gmail.com>
2013-02-27 20:06:15 +0100

    preparing for ajax

--- styria/single_image/forms.py
+++ styria/single_image/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 
-from models import Image
+from models import Image, Rating, Comment
 
 
 class UploadImageForm(forms.ModelForm):
     class Meta:
         model = Image
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+
+
+class RatingForm(forms.ModelForm):
+    class Meta:
+        model = Rating
